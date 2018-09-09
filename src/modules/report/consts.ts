9b3130590fc3505a5b9a45541053e1bf6bb27223
Tom Lingham <tjlingham@gmail.com>
2018-09-09 15:28:55 +1000

    Fix breaking version in test.

--- src/modules/report/consts.ts
+++ src/modules/report/consts.ts
@@ -4,4 +4,5 @@ import * as pjson from "../../../package.json";
 export const WORK_DIR = process.cwd();
 export const LOCKFILE_NAME = "treeshackle-lock.json";
 export const LOCKFILE_PATH = path.resolve(WORK_DIR, LOCKFILE_NAME);
-export const VERSION = pjson.version;
+export const VERSION =
+  process.env.NODE_ENV === "test" ? "1.0.0-test" : pjson.version;
